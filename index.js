9f6487ccd8563b53fcf054559821627d463a689e
azam <azamali6995@gmail.com>
2024-08-15 11:33:12 +0500

    modified code

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
 #!/usr/bin/env node
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+
+import ButtonComponent from './src/component/ButtonComponent'
+import CheckBox from './src/component/CheckBox';
+import DropDown from './src/component/DropDown';
+import DynamicProgressBar from './src/component/DynamicProgressBar';
+import DynamicSlider from './src/component/DynamicSlider';
+import FloatingLabelTextInput from './src/component/FloatingLabelTextInput';
+import InputText from './src/component/InputText';
+import LoadingPage from './src/component/LoadingPage';
+import ModalComponent from './src/component/ModalComponent';
+import PickerDate from './src/component/PickerDate';
+import PickerTime from './src/component/PickerTime';
+import RadioButton from './src/component/ReadioButton';
+import TextComponent from './src/component/TextComponent';
+
+
+
+
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
+
+export {
+    ButtonComponent,
+    CheckBox,
+    DropDown,
+    DynamicProgressBar,
+    DynamicSlider,
+    FloatingLabelTextInput,
+    InputText,
+    LoadingPage,
+    ModalComponent,
+    PickerDate,
+    PickerTime,
+    RadioButton,
+    TextComponent,
+};
+
+
+
+
+// AppRegistry.registerComponent(appName, () => App);
